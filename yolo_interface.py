93409a6e7d60de7231d1dd735bd7507d701f756c
ReuvenEliyahu <reuven120@gmail.com>
2025-04-19 09:55:02 +0300

    Update yolo_interface.py

--- yolo_interface.py
+++ yolo_interface.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO(r"C:\Users\97254\Desktop\Football Analysis\models\best.pt")
 
 
-results = model.predict(r"C:\Users\97254\Desktop\Football Analysis\08fd33_4.mp4", save=True)
+results = model.predict(r"YOUR LOCATION", save=True)
 print(results[0])
 print('=====================================')
 for box in results[0].boxes:
